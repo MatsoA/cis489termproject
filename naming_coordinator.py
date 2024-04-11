441ff63af46fcbff882e5e1956b32834f952fc10
evelyns1 <94881114+evelyns1@users.noreply.github.com>
2024-04-11 19:45:03 -0400

    Update naming_coordinator.py

--- naming_coordinator.py
+++ naming_coordinator.py
@@ -1,4 +1,11 @@
 import sys
+import socket
+
+UDP_IP = "192.168.43.255"
+UDP_PORT = 5005
+sock = socket.socket(socket.AF_INET,
+                         socket.SOCK_DGRAM)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
 class Naming:
     semantic_map = {
@@ -22,17 +29,18 @@ class Naming:
 
     def broadcast_request_and_receive(sub_routes):
         for route in sub_routes:
-            #TODO: code to broadcast http request with subroute 
-            pass
+            # code to broadcast http request with subroute
+            sock.sendto(bytes(route, encoding='utf-8'), (UDP_IP, UDP_PORT))
 
-        #TODO: recieve data and return all output recieved
-        #TODO: filter responses of the same topic (choose one)
+        sock.bind((UDP_IP, UDP_PORT))
+        # recieve data and return all output recieved
+        while True:
+            data, addr = sock.recvfrom(1024)
+            return data
+        # filter responses of the same topic (choose one)
 
         #for testing
         print(sub_routes) 
-    
-    
-
 
 #test usage:
 Naming.serve_route(sys.argv[1])
